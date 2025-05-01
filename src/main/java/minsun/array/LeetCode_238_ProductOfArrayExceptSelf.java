fe93cb7c3c7e866d176a7d16dcc61396b1792d74
이민선 <woooooooow22@gmail.com>
2025-05-01 09:04:59 +0900

    LeetCode_238_ProductOfArrayExceptSelf.java

---
+++ src/main/java/minsun/array/LeetCode_238_ProductOfArrayExceptSelf.java
@@ -0,0 +1,25 @@
+package minsun.array;
+
+import java.util.*;
+
+class LeetCode_238_ProductOfArrayExceptSelf {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] answer = new int[n];
+        Arrays.fill(answer, 1);
+        int[] accMul = new int[n];
+
+        accMul[n - 1] = nums[n - 1];
+        for (int i=1;i<n;i++){
+            answer[i] = answer[i - 1] * nums[i - 1];
+            accMul[n - i - 1] = accMul[n - i] * nums[n - i - 1];
+        }
+
+        for (int i=0;i<n - 1;i++){
+            if (i == 0) answer[i] *= accMul[i + 1];
+            else answer[i] *= accMul[i + 1];
+        }
+
+        return answer;
+    }
+}
