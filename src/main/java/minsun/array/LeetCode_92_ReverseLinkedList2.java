a18d773d02a30f6b81723524e4fa318e9353d28b
이민선 <woooooooow22@gmail.com>
2025-05-12 22:08:24 +0900

    LeetCode_92_ReverseLinkedList2.java

---
+++ src/main/java/minsun/array/LeetCode_92_ReverseLinkedList2.java
@@ -0,0 +1,32 @@
+package minsun.array;
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class LeetCode_92_ReverseLinkedList2 {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode prev = dummy;
+
+        for (int i=0;i<left - 1;i++){
+            prev = prev.next;
+        }
+
+        ListNode cur = prev.next;
+
+        for (int i=left;i<right;i++){
+            ListNode next = cur.next;
+            cur.next = next.next;
+            next.next = prev.next;
+            prev.next = next;
+        }
+        // 왜 dummy.next는 되고 head는 안되는가?
+        return head;
+    }
+}
