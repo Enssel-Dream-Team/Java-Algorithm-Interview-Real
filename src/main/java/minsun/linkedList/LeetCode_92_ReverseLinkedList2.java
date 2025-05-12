4b8325531666e0b72e78df08934815c12b4244e7
이민선 <woooooooow22@gmail.com>
2025-05-12 22:22:54 +0900

    정리

--- src/main/java/minsun/linkedList/LeetCode_92_ReverseLinkedList2.java
+++ src/main/java/minsun/linkedList/LeetCode_92_ReverseLinkedList2.java
@@ -1,14 +1,8 @@
 package minsun.array;
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+
+
+
+
 class LeetCode_92_ReverseLinkedList2 {
     public ListNode reverseBetween(ListNode head, int left, int right) {
         ListNode dummy = new ListNode(0, head);
