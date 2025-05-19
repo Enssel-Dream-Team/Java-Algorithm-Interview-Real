772f60949c9770a5dae9372a4080e3ea42cb6d41
da9dac <da9dac@gmail.com>
2025-05-19 20:03:51 +0900

    21번 풀이

--- src/main/java/yjham/stackQueue/LeetCode_316_RemoveDuplicateLetters.java
+++ src/main/java/yjham/stackQueue/LeetCode_316_RemoveDuplicateLetters.java
@@ -1,8 +1,39 @@
 package yjham.stackQueue;
 
+import java.util.Stack;
+
 public class LeetCode_316_RemoveDuplicateLetters {
 
 	public String removeDuplicateLetters(String s) {
-		return null;
+		int len = s.length();
+
+		int[] count = new int[26];
+		boolean[] isExist = new boolean[26];
+
+		for (int i = 0; i < len; i++) {
+			count[s.charAt(i) - 'a']++;
+		}
+
+		Stack<Character> stack = new Stack<>();
+
+		for (int i = 0; i < len; i++) {
+			char c = s.charAt(i);
+			int idx = c - 'a';
+
+			count[idx]--;
+
+			if (isExist[idx]) continue;
+
+			while (!stack.isEmpty() && stack.peek() > c && count[stack.peek() - 'a'] > 0) {
+				isExist[stack.pop() - 'a'] = false;
+			}
+
+			stack.push(c);
+			isExist[idx] = true;
+		}
+
+		return stack.stream()
+			.collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
+			.toString();
 	}
 }
