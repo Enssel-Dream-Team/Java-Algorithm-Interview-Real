d6d17f64753f1eb27de3c60fa7b0da8d1c3f4014
이민선 <woooooooow22@gmail.com>
2025-05-19 21:13:04 +0900

    LeetCode_622_DesignCircularQueue

--- src/main/java/minsun/stackQueue/LeetCode_622_DesignCircularQueue.java
+++ src/main/java/minsun/stackQueue/LeetCode_622_DesignCircularQueue.java
@@ -1,4 +1,47 @@
 package minsun.stackQueue;
 
-public class LeetCode_622_DesignCircularQueue {
+class LeetCode_622_DesignCircularQueue {
+    int[] circularQ;
+    int size = 0;
+    int idx = -1;
+    public LeetCode_622_DesignCircularQueue(int k) {
+        circularQ = new int[k];
+    }
+
+    public boolean enQueue(int value) {
+        if (size < circularQ.length) {
+            size++;
+            circularQ[++idx % circularQ.length] = value;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean deQueue() {
+        if (size > 0) {
+            circularQ[(idx - size-- + circularQ.length + 1) % circularQ.length] = 0;
+            return true;
+        }
+        return false;
+    }
+
+    public int Front() {
+        if (size > 0)
+            return circularQ[(idx - size + circularQ.length + 1) % circularQ.length];
+        return -1;
+    }
+
+    public int Rear() {
+        if (size > 0)
+            return circularQ[idx % circularQ.length];
+        return -1;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean isFull() {
+        return size == circularQ.length;
+    }
 }
