232d176d194d5db171f2db94ff76e16a51576ce6
Chae-1 <coguddlf1000@naver.com>
2025-05-20 17:58:40 +0900

    complete two linked list

--- src/main/java/hyeongil/linkedlist/LeetCode_328_OddEvenLinkedList.java
+++ src/main/java/hyeongil/linkedlist/LeetCode_328_OddEvenLinkedList.java
@@ -1,5 +1,24 @@
 package hyeongil.linkedlist;
 
 public class LeetCode_328_OddEvenLinkedList {
+	public ListNode oddEvenList(ListNode head) {
+		// 짝수 번째 노드를 이루고 있는 노드, 홀수 번째 노드를 이루고 있는 노드
+		if (head == null) {
+			return head;
+		}
+		ListNode odd = head;
+		ListNode even = head.next;
+		ListNode evenP = even;
 
+		while (evenP != null && evenP.next != null) {
+			odd.next = evenP.next;
+			evenP.next = evenP.next.next;
+
+			evenP = evenP.next;
+			odd = odd.next;
+		}
+
+		odd.next = even;
+		return head;
+	}
 }
