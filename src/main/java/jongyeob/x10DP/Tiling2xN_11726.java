624ca4c7b83f30edcb006f499a1a2b5d45aba0a2
yujongyeop <230233@naver.com>
2025-10-15 13:37:08 +0900

    11726번 2xN 타일링(백준)
    [개요]
    - 2xN 크기의 공간에 2x1 크기 블록을 채울 수 있는 경우의 수 구하기
    
    [해결 방식]
    - 점화식: fx(i) = (fx(i-1) + fx(i-2)) % 10007

---
+++ src/main/java/jongyeob/x10DP/Tiling2xN_11726.java
@@ -0,0 +1,17 @@
+package jongyeob.x10DP;
+
+import java.util.Scanner;
+
+public class Tiling2xN_11726 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        long[] dp = new long[1001];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i < n + 1; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+        }
+        System.out.println(dp[n] % 10007);
+    }
+}
