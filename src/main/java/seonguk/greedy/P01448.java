c1de9cf4db4fc955a1c1c4bf38569f1e5464acd8
Seonguk-j <deas124@gmail.com>
2026-03-30 19:41:26 +0900

    1448번 삼각형 만들기(백준)

---
+++ src/main/java/seonguk/greedy/P01448.java
@@ -0,0 +1,31 @@
+package seonguk.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class P01448 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+        int ans = -1;
+
+        for (int i = N - 1; i >= 2; i--) {
+            if (arr[i] < arr[i - 1] + arr[i - 2]) {
+                ans = arr[i] + arr[i - 1] + arr[i - 2];
+                break;
+            }
+        }
+        System.out.println(ans);
+        br.close();
+    }
+}
